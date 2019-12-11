1a39868ac3d38ca804101e13ddf7c99740ff2d73
patrickgillette <58642860+patrickgillette@users.noreply.github.com>
2019-12-10 21:55:28 -0600

    Changed map

--- 4x Game/Assets/HexMap.cs
+++ 4x Game/Assets/HexMap.cs
@@ -6,13 +6,14 @@ public class HexMap : MonoBehaviour
 {
     public bool randomMap;
     public bool blankMap;
+    public bool testMap;
     // Start is called before the first frame update
     void Start()
     {
         GenerateMap();
     }
     public GameObject HexPrefab;
-    
+    public GameObject HexStar;
     public Material MatSpace;
     public Material MatPlanet;
     public Material MatStar;
@@ -22,14 +23,50 @@ public class HexMap : MonoBehaviour
     public Mesh MeshPlanet;
     public Mesh MeshStar;
     public Mesh MeshAsteroid;
+    private Hex[,] hexes;
+    private Dictionary<Hex, GameObject> HexToGameObjectMap;
+    
 
-
+    
     public Material[] HexMaterials;
     int numRows = 100;
     int numColumns = 100;
 
+    public Hex GetHexAt(int x, int y)
+    {
+        if(hexes == null)
+        { Debug.LogError("Hexes not yet instantiated");
+            return null; }
+        return hexes[x, y];
+
+    }
+
+
     public void GenerateMap()
     {
+        hexes = new Hex[ numColumns , numRows];
+        HexToGameObjectMap = new Dictionary<Hex, GameObject>();
+
+        if (testMap == true)
+        {
+            generateBlankMap();
+
+            ElevateArea(21, 15, 1);
+
+            UpdateHexVisuals();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
         if (blankMap == true)
         {
             generateBlankMap();   
@@ -42,42 +79,61 @@ public class HexMap : MonoBehaviour
 
             for (int column = 0; column < numColumns; column++)
             {
-
                 hexModvar = 0;
-
-
-
                 for (int row = 0; row < numRows; row++)
                 {
                     int hexMod = hexModvar;
-
-
-
                     Hex h = new Hex(column - hexMod, row);
                     GameObject hexGO = Instantiate(HexPrefab, h.Position(), Quaternion.identity, this.transform);
                     MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
                     //mr.material = MatSpace;
-                     mr.material = HexMaterials[Random.Range(0, HexMaterials.Length)];
-               
+                     mr.material = HexMaterials[Random.Range(0, HexMaterials.Length)];           
                     if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
                     {
-                        Debug.Log(rowCounter);
+                       // Debug.Log(rowCounter);
                         hexModvar++;
                     }
                     rowCounter++;
                     if (rowCounter == 10)
-
                     { rowCounter = rowCounter / 10 - 1; }
 
-
                 }
-
-
             }
 
+            for (int column = 0; column < numColumns; column++)
+            {
+                hexModvar = 0;
+                for (int row = 0; row < numRows; row++)
+                {
+                    int hexMod = hexModvar;
+
+                   
+                    
+                        int starGen = Random.Range(1, 100);
+                        if (starGen < 10)
+                        {
+                        
+                       
+                    }
+
+                  //  }
 
 
 
+
+
+                    if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
+                    {
+                     
+                        hexModvar++;
+                    }
+                    rowCounter++;
+                    if (rowCounter == 10)
+                    { rowCounter = rowCounter / 10 - 1; }
+                }
+            }
+
+            
 
 
 
@@ -96,12 +152,20 @@ public class HexMap : MonoBehaviour
             hexModvar = 0;
             for (int row = 0; row < numRows; row++)
             {
-                //generate blank map
                 int hexMod = hexModvar;
                 Hex h = new Hex(column - hexMod, row);
+                h.Elevation = -1;
+                
+                hexes[column, row] = h;
+
+                
+
                 GameObject hexGO = Instantiate(HexPrefab, h.Position(), Quaternion.identity, this.transform);
+                HexToGameObjectMap[h] = hexGO;
                 MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
                 mr.material = MatSpace;
+
+                //Offsets the edges
                 if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
                 {
                     Debug.Log(rowCounter);
@@ -114,6 +178,81 @@ public class HexMap : MonoBehaviour
             }
         }
     }
+
+    void ElevateArea(int q, int r, int radius)
+    {
+        for (int x = -radius; x < radius; x++)
+        {
+            for (int y = -radius; y < radius; y++)
+            {
+                Hex h = GetHexAt(q + x, r + y);
+                h.Elevation = 0.5f;
+            }
+        }
+
+    }
+
+    public void UpdateHexVisuals()
+    {
+  
+            for (int column = 0; column < numColumns; column++)
+            {
+                hexModvar = 0;
+                for (int row = 0; row < numRows; row++)
+                {
+                Hex h = hexes[column, row];
+                GameObject hexGO = HexToGameObjectMap[h];
+                MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
+                if(h.Elevation >= 0)
+                {
+                    mr.material = MatStar;
+                }
+                else
+                mr.material = MatSpace;
+
+                
+
+
+
+
+
+
+                //Offsets the edges
+                if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
+                    {
+                       
+                        hexModvar++;
+                    }
+                    rowCounter++;
+                    if (rowCounter == 10)
+
+                    { rowCounter = rowCounter / 10 - 1; }
+                }
+            }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
@@ -131,3 +270,22 @@ public class HexMap : MonoBehaviour
 
 //if (row == 0 || row == 2 || row == 4 || row == 6 || row == 8)
 //                   { hexModvar++; }
+
+/*
+ * for (int column = 0; column < numColumns; column++)
+        {
+            hexModvar = 0;
+            for (int row = 0; row < numRows; row++)
+            {
+
+                //Offsets the edges
+                if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
+                {
+                    hexModvar++;
+                }
+                rowCounter++;
+                if (rowCounter == 10)
+                { rowCounter = rowCounter / 10 - 1; }
+            }
+        }
+*/
