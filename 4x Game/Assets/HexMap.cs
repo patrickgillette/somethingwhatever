43f2429510a3ab02d3d839b9d2869b8dd3fd41c6
patrickgillette <58642860+patrickgillette@users.noreply.github.com>
2019-12-12 01:15:39 -0600

    Map

--- 4x Game/Assets/HexMap.cs
+++ 4x Game/Assets/HexMap.cs
@@ -44,109 +44,134 @@ public class HexMap : MonoBehaviour
 
     public void GenerateMap()
     {
-        hexes = new Hex[ numColumns , numRows];
+        hexes = new Hex[numColumns, numRows];
         HexToGameObjectMap = new Dictionary<Hex, GameObject>();
+
+        if (blankMap == true)
+        {
+            generateBlankMap();
+        }
+
+
 
         if (testMap == true)
         {
             generateBlankMap();
 
-            ElevateArea(21, 15, 1);
-
-            UpdateHexVisuals();
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-        if (blankMap == true)
-        {
-            generateBlankMap();   
-            }
-
-        //generate random map
-        if (randomMap == true)
-        {
-           // generateBlankMap();
-
+            int starGen;
             for (int column = 0; column < numColumns; column++)
             {
                 hexModvar = 0;
                 for (int row = 0; row < numRows; row++)
                 {
                     int hexMod = hexModvar;
-                    Hex h = new Hex(column - hexMod, row);
-                    GameObject hexGO = Instantiate(HexPrefab, h.Position(), Quaternion.identity, this.transform);
-                    MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
-                    //mr.material = MatSpace;
-                     mr.material = HexMaterials[Random.Range(0, HexMaterials.Length)];           
-                    if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
+
+                    starGen = Random.Range(1, 100);
+                    if (starGen < 2 && column > 3 && column < numColumns - 3 && row > 3 && row < numRows - 3)
                     {
-                       // Debug.Log(rowCounter);
-                        hexModvar++;
-                    }
-                    rowCounter++;
-                    if (rowCounter == 10)
-                    { rowCounter = rowCounter / 10 - 1; }
+                        ElevateArea(row, column, 5);
 
-                }
-            }
 
-            for (int column = 0; column < numColumns; column++)
-            {
-                hexModvar = 0;
-                for (int row = 0; row < numRows; row++)
-                {
-                    int hexMod = hexModvar;
 
-                   
-                    
-                        int starGen = Random.Range(1, 100);
-                        if (starGen < 10)
+
+
+
+
+
+
+                        //Offsets the edges
+                        if (rowCounter % 2 == 0)
                         {
-                        
-                       
+                            //Debug.Log(rowCounter);
+                            hexModvar++;
+                        }
+                        rowCounter++;
+
+
                     }
-
-                  //  }
-
-
-
-
-
-                    if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
-                    {
-                     
-                        hexModvar++;
-                    }
-                    rowCounter++;
-                    if (rowCounter == 10)
-                    { rowCounter = rowCounter / 10 - 1; }
                 }
+
+               
+
+
+
+
+                UpdateHexVisuals();
+
             }
 
-            
+
+
+
+
+            //generate random map
+            if (randomMap == true)
+            {
+                // generateBlankMap();
+
+                for (int column = 0; column < numColumns; column++)
+                {
+                    hexModvar = 0;
+                    for (int row = 0; row < numRows; row++)
+                    {
+                        int hexMod = hexModvar;
+                        Hex h = new Hex(column - hexMod, row);
+                        GameObject hexGO = Instantiate(HexPrefab, h.Position(), Quaternion.identity, this.transform);
+                        MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
+                        //mr.material = MatSpace;
+                        mr.material = HexMaterials[Random.Range(0, HexMaterials.Length)];
+                        if (rowCounter % 2 == 0)
+                        {
+                            //Debug.Log(rowCounter);
+                            hexModvar++;
+                        }
+                        rowCounter++;
+
+                    }
+                }
+
+                for (int column = 0; column < numColumns; column++)
+                {
+                    hexModvar = 0;
+                    for (int row = 0; row < numRows; row++)
+                    {
+                        int hexMod = hexModvar;
+
+
+
+                        
 
 
 
 
 
 
+
+                        if (rowCounter % 2 == 0)
+                        {
+                            //Debug.Log(rowCounter);
+                            hexModvar++;
+                        }
+                        rowCounter++;
+                    }
+                }
+
+
+
+
+
+
+
+
+
+            }
 
         }
-        
     }
 
     public void generateBlankMap()
     {
+        int starGen;
         for (int column = 0; column < numColumns; column++)
         {
             hexModvar = 0;
@@ -165,31 +190,33 @@ public class HexMap : MonoBehaviour
                 MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
                 mr.material = MatSpace;
 
-                //Offsets the edges
-                if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
-                {
-                    Debug.Log(rowCounter);
-                    hexModvar++;
-                }
-                rowCounter++;
-                if (rowCounter == 10)
+               
 
-                { rowCounter = rowCounter / 10 - 1; }
+                //Offsets the edges
+                               if (rowCounter%2 == 0)
+                               {
+                                   //Debug.Log(rowCounter);
+                                   hexModvar++;
+                               }
+                               rowCounter++;
+                               
+                    
             }
         }
     }
 
     void ElevateArea(int q, int r, int radius)
     {
-        for (int x = -radius; x < radius; x++)
-        {
-            for (int y = -radius; y < radius; y++)
-            {
-                Hex h = GetHexAt(q + x, r + y);
-                h.Elevation = 0.5f;
-            }
-        }
+        Hex centerHex = GetHexAt(q, r);
+        Hex[] areaHexes = GetHexesWithinRadiusOf(centerHex, radius,q,r);
+            
+                foreach(Hex h in areaHexes)
+                {
+                h.Elevation = 1;
+                }
+        
 
+        centerHex.Elevation = 2;
     }
 
     public void UpdateHexVisuals()
@@ -198,19 +225,23 @@ public class HexMap : MonoBehaviour
             for (int column = 0; column < numColumns; column++)
             {
                 hexModvar = 0;
-                for (int row = 0; row < numRows; row++)
-                {
+            for (int row = 0; row < numRows; row++)
+            {
                 Hex h = hexes[column, row];
                 GameObject hexGO = HexToGameObjectMap[h];
                 MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
-                if(h.Elevation >= 0)
+                if (h.Elevation >= 2)
                 {
                     mr.material = MatStar;
                 }
+                else if (h.Elevation == 1)
+                {
+                    mr.material = MatPlanet;
+                }
                 else
-                mr.material = MatSpace;
+                    mr.material = MatSpace;
 
-                
+
 
 
 
@@ -219,16 +250,47 @@ public class HexMap : MonoBehaviour
 
                 //Offsets the edges
                 if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
-                    {
-                       
-                        hexModvar++;
-                    }
-                    rowCounter++;
-                    if (rowCounter == 10)
+                {
 
-                    { rowCounter = rowCounter / 10 - 1; }
+                    hexModvar++;
                 }
-            }
+                rowCounter++;
+                if (rowCounter == 10)
+
+                { rowCounter = rowCounter / 10 - 1; }
+            
+                }
+            }      
+    }
+
+    public Hex[] GetHexesWithinRadiusOf(Hex centerHex, int radius, int row, int column)
+    {
+    
+        List<Hex> results = new List<Hex>();
+
+        
+      
+              for (int dx = row -radius ; dx <= row + radius ; dx++)
+               {
+            
+                   for (int dy = column-radius  ; dy <= column + radius ; dy++)
+                   {
+
+                    if (Mathf.Sqrt(((Mathf.Abs(row - dx  )* Mathf.Abs(row - dx  )) + Mathf.Abs(column - dy ) * Mathf.Abs(column - dy ) ) ) +1 <= radius)
+                    {
+                    results.Add(hexes[dx, dy]);
+                    }
+                
+                       
+                  
+                 
+                     
+
+                   }
+
+               }
+        
+        return results.ToArray();
         
     }
 
@@ -236,11 +298,10 @@ public class HexMap : MonoBehaviour
 
 
 
+    //for (int dy = Mathf.Max(-radius, -dx - radius + 1); dy<Mathf.Min(radius, -dx + radius); dy++)
 
 
-
-
-
+   
 
 
 
@@ -279,13 +340,24 @@ public class HexMap : MonoBehaviour
             {
 
                 //Offsets the edges
-                if (rowCounter == 0 || rowCounter == 2 || rowCounter == 4 || rowCounter == 6 || rowCounter == 8)
-                {
-                    hexModvar++;
-                }
-                rowCounter++;
-                if (rowCounter == 10)
-                { rowCounter = rowCounter / 10 - 1; }
+                if (rowCounter%2 == 0)
+                               {
+                                   //Debug.Log(rowCounter);
+                                   hexModvar++;
+                               }
+                               rowCounter++;
             }
         }
+
+
+
+
+
+    //star rng
+     starGen = Random.Range(1, 100);
+                if (starGen < 5)
+                {
+                    ElevateArea(21, 15, 4);
+
+                }
 */
